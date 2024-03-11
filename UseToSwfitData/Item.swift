4a657f5db2ca7a820fa45c7948dab787b37af734
burakekinci7 <ekincib015@gmail.com>
2024-03-11 15:13:10 +0300

    Initial Commit

---
+++ UseToSwfitData/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  UseToSwfitData
+//
+//  Created by Ramazan Burak Ekinci on 11.03.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
